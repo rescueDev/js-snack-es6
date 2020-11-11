337cc869ea123067bf67ca61de40a66f78fcb423
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-11 15:56:23 +0100

    esercizio 2 finito

--- JSnack7/js/script.js
+++ JSnack7/js/script.js
@@ -1,9 +1,9 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
-var biciclette = [
+const biciclette = [
   {
     nome: "atala",
-    peso: 50 + " kg",
+    peso: 50,
   },
   {
     nome: "trix",
@@ -23,15 +23,22 @@ var biciclette = [
   },
 ];
 
-//ciclo for in per ciclare tutti i valori peso
-for (var i = 0; i < biciclette.length; i++) {
-  console.log(biciclette[i].nome + " : " + biciclette[i].peso);
-}
-
-// ordino le bici nell'array in base al peso
-biciclette.sort(function (a, b) {
-  return a.peso - b.peso;
+//ciclo forEach per ciclare tutti i valori peso
+biciclette.forEach((element) => {
+  console.log(`
+  La bicicletta ${element.nome} pesa ${element.peso} kg
+  `);
 });
 
-var min = biciclette[0];
-console.log(min); //mostro la bicicletta con il peso minore
+// ordino le bici nell'array in base al peso
+//implemento Arrow function al posto di una normale anonima funzione
+biciclette.sort((a, b) => {
+  return a.peso - b.peso; //essendo tra le graffe metto sempre return
+});
+
+//applico filter method per estrapolare la bici più leggera in un nuovo array
+const biciLeggera = biciclette.filter((element) => {
+  return element === biciclette[0]; // indice 0 dell'array corrisponde alla bici più leggera dopo averle ordinate con il sort
+});
+// var min = biciclette[0];
+console.log(biciLeggera); //mostro la bicicletta con il peso minore
