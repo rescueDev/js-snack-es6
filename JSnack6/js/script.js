01e5406d7eee88837a674f8c59633b0d32340d3e
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-11 15:23:53 +0100

    primo Jsnack con nuovi argomenti

--- JSnack6/js/script.js
+++ JSnack6/js/script.js
@@ -2,9 +2,13 @@
 Creare un oggetto palla che abbia le seguenti proprietà. Nome = palla
 Peso = 10 */
 
-var palla = {
-  nome: "palla",
-  peso: 10,
+//Assegno esternamente i valori che riprenderà nella costante
+let nome = "palla";
+let peso = 10;
+
+const palla = {
+  nome, //nella costante le chiavi siccome coincidono (nome : nome e peso:peso) posso semplicemente omettere il valore della proprietà
+  peso,
 };
 
 console.log(palla);
@@ -18,4 +22,12 @@ var modifica = parseInt(prompt("inserisci nuovo peso palla")); //creo variabile 
 palla["peso"] = modifica; //modifico il valore di peso con il valore inserito nel prompt
 
 console.log(palla); //vedo il nuovo oggetto con valore modificato
-console.log("Nuovo peso :", palla.peso); //nuovo peso
+
+//prova TEMPLATE LITERAL in output
+//nuovo peso
+console.log(
+  `
+  Il nuovo peso della palla è:
+  ${palla.peso} kg
+  `
+);
