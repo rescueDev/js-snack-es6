ae76f88631945f445e08e63e92cf3d6c5fbfb40e
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-11 18:45:06 +0100

    ultimo es finito

--- JSnack10/js/script.js
+++ JSnack10/js/script.js
@@ -10,39 +10,59 @@ Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (qu
 una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale */
 
 //array di partenza
-var arrayObj = [
-  {name: 'Poppy', type: 'tshirt', color: 'red'},
-  {name: 'Jumping', type: 'occhiali', color: 'blue'},
-  {name: 'CrissCross', type: 'scarpe', color: 'black'},
-  {name: 'Jenny', type: 'borsa', color: 'pink'},
+const arrayObj = [
+  { name: "Poppy", type: "tshirt", color: "red" },
+  { name: "Jumping", type: "occhiali", color: "blue" },
+  { name: "CrissCross", type: "scarpe", color: "black" },
+  { name: "Jenny", type: "borsa", color: "pink" },
 ];
 
-console.log("Array prima dell'aggiunta: " , arrayObj);  //stampo array di partenza
+console.log("Array prima dell'aggiunta: ", arrayObj); //stampo array di partenza
 
-//creo copia arrayObj 
-var arrayObj2 = [
-  {name: 'Poppy', type: 'tshirt', color: 'red'},
-  {name: 'Jumping', type: 'occhiali', color: 'blue'},
-  {name: 'CrissCross', type: 'scarpe', color: 'black'},
-  {name: 'Jenny', type: 'borsa', color: 'pink'},
-];
+//creo copia arrayObj
+const arrayObjCopy = JSON.parse(JSON.stringify(arrayObj));
+console.log(arrayObjCopy); //mostro nuovo array copia
 
-
-//ciclo fra tutti gli indici dell'array e aggiungo la proprietà letter con funzione randLetter associata
-for (var i = 0; i < arrayObj2.length;i++){
-    arrayObj2[i].letter = randLetter();
+//ciclo for in per assegnare lettera random
+for (var key in arrayObjCopy) {
+  arrayObjCopy[key].letter = randLetter();
 }
- 
-console.log("Array dopo l'aggiunta: " , arrayObj2); // mostro il nuovo array con letter
 
-
+console.log(arrayObjCopy);
 
 /* =================================== FUNZIONI GENERALI =========================================================================== */
 
 //funzione per generare lettera casuale
 
 function randLetter() {
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var letter = letters[Math.floor(Math.random() * letters.length)];
-    return letter
+  var letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var letter = letters[Math.floor(Math.random() * letters.length)];
+  return letter;
 }
