af07fc949e19df3f7c266f40009623d59b8f8165
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-12 17:00:16 +0100

    clone array + aggiunta bonus ordino classifica per punti

--- JSnack8/js/script.js
+++ JSnack8/js/script.js
@@ -39,7 +39,18 @@ const squadre = [
 
 console.log(squadre); //vedo l'oggetto prima delle operazioni
 
-//ciclo forEach che mi popola i valori di punti e falliSubiti con numeri random
+//clono l'array di squadre per non modificare i punti e falli subiti dell'array origine
+//uso map con object assign
+const squadreCopy = squadre.map((u) => {
+  const copiedElement = Object.assign({}, u);
+  copiedElement.punti = randomGen();
+  copiedElement.falliSubiti = randomGen();
+  return copiedElement;
+});
+
+console.log(squadreCopy);
+
+/* //ciclo forEach che mi popola i valori di punti e falliSubiti con numeri random
 squadre.forEach((element) => {
   return [(element.punti = randomGen())], [(element.falliSubiti = randomGen())];
 });
@@ -51,7 +62,17 @@ squadre.forEach((element) => {
   Punti: ${element.punti}
   Falli subiti: ${element.falliSubiti}
   `);
+}); */
+
+//BONUS
+//Ordino secondo i punti le squadre come una classifica
+
+squadreCopy.forEach((element) => {
+  squadreCopy.sort((a, b) => b.punti - a.punti);
+  return squadreCopy;
 });
+
+console.log("Classifica squadre: ", squadreCopy);
 
 // funzione per generare numeri random
 function randomGen() {
